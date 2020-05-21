1daf736af2164e74be74072519997477ee120bd0
Saurabh Bharti <30685271+Iamsbharti@users.noreply.github.com>
2020-05-21 10:57:33 +0530

    completed check logic

--- src/index.js
+++ src/index.js
@@ -1,7 +1,23 @@
 import "./styles.css";
 const checkboxes = document.querySelectorAll(".inbox input[type='checkbox']");
-
+let lastChecked;
 function selection(e) {
-  console.log(e);
+  //check if the shift key is down && they are checking it
+  let inBetween = false;
+  if (e.shiftKey && this.checked) {
+    //loop over every checkbox and find initial and final box
+    //to be checked
+    checkboxes.forEach(box => {
+      console.log(box);
+      if (box === this || box === lastChecked) {
+        inBetween = !inBetween;
+        console.log("Start to check then between");
+      }
+      if (inBetween) {
+        box.checked = true;
+      }
+    });
+  }
+  lastChecked = this;
 }
 checkboxes.forEach(item => item.addEventListener("click", selection));
