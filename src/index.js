2403cfd6e0965c03372daf7eb32f9f8dea65c8b7
Saurabh Bharti <30685271+Iamsbharti@users.noreply.github.com>
2020-05-21 10:48:43 +0530

    styles and js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
 import "./styles.css";
+const checkboxes = document.querySelectorAll(".inbox input[type='checkbox']");
+
+function selection(e) {
+  console.log(e);
+}
+checkboxes.forEach(item => item.addEventListener("click", selection));
